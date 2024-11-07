f7e3eca765f0ba29d12e611a2f85c8964b4335fc
EternityFox <tkachevnikolay96@gmail.com>
2024-11-07 14:05:46 +1000

    added send email

--- public_html/model/admin_model.php
+++ public_html/model/admin_model.php
@@ -155,6 +155,7 @@ function editQuestion()
     $email = isset($_POST['email']) && $_POST['email'] ? $_POST['email'] : "no-reply@bookmirs.ru";
     $question = $_POST['question'];
     $answer = $_POST['answer'];
+    $send_mail = $_POST['send_mail'];
     $now = time();
     $updated_at = date('Y-m-d H:i:s', $now);
     $active = $_POST['active'];
@@ -162,8 +163,60 @@ function editQuestion()
     if ($question_id) {
         $query = "UPDATE questions SET name='$name', email='$email', question='$question', answer='$answer', updated_at='$updated_at', active='$active' WHERE id='$question_id'";
         $res = mysql_query($query) or die(mysql_error());
+        if ($res == 1 && $send_mail === "true" && $email) {
+            $message = "Здравствуйте,". $name."! <br>"."Спасибо за ваш вопрос на нашем сайте. Мы рады, что вы обратились к нам и готовы помочь вам с вашим вопросом:" .$question.".<br>Наш ответ: ". $answer;
+            sendMail($name, $email, $message);
+        }
     }
 }
+
+function sendMail($name, $email, $message)
+{
+    function mime_header_encode($str, $data_charset, $send_charset)
+    {
+        if ($data_charset != $send_charset)
+            $str = iconv($data_charset, $send_charset . '//IGNORE', $str);
+        return ('=?' . $send_charset . '?B?' . base64_encode($str) . '?=');
+    }
+
+    class TEmail
+    {
+        public $from_email;
+        public $from_name;
+        public $to_email;
+        public $to_name;
+        public $subject;
+        public $data_charset = 'UTF-8';
+        public $send_charset = 'windows-1251';
+        public $body = '';
+        public $type = 'text/plain';
+
+        function send()
+        {
+            $dc = $this->data_charset;
+            $sc = $this->send_charset;
+            $enc_to = mime_header_encode($this->to_name, $dc, $sc) . ' <' . $this->to_email . '>';
+            $enc_subject = mime_header_encode($this->subject, $dc, $sc);
+            $enc_from = mime_header_encode($this->from_name, $dc, $sc) . ' <' . $this->from_email . '>';
+            $enc_body = $dc == $sc ? $this->body : iconv($dc, $sc . '//IGNORE', $this->body);
+            $headers = '';
+            $headers .= "Mime-Version: 1.0\r\n";
+            $headers .= "Content-type: " . $this->type . "; charset=" . $sc . "\r\n";
+            $headers .= "From: " . $enc_from . "\r\n";
+            return mail($enc_to, $enc_subject, $enc_body, $headers);
+        }
+
+    }
+    $emailgo= new TEmail;
+    $emailgo->from_email= 'kds@bookmirs.ru';
+    $emailgo->from_name= 'Администрация сайта bookmirs.ru';
+    $emailgo->to_email= $email;
+    $emailgo->to_name= $name;
+    $emailgo->subject= 'Ответ на ваш вопрос на сайте bookmirs.ru';
+    $emailgo->body= $message;
+    $emailgo->send();
+}
+
 function getCoupons($limit = '', $orderBy = 'updated_at')
 {
     if ($limit) {
@@ -177,6 +230,7 @@ function getCoupons($limit = '', $orderBy = 'updated_at')
     }
     return $coupons;
 }
+
 function addCoupon()
 {
     $error = '';
@@ -187,6 +241,7 @@ function addCoupon()
 			VALUES ('$code', '$createt_add')";
     $res = mysql_query($query) or die(mysql_error());
 }
+
 function getQuestions($limit = '', $orderBy = 'created_at')
 {
     if ($limit) {
